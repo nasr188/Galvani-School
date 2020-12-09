df3638ab2831651e260e6b20945a82ef6565066e
nasr188 <nasraldin424@gmail.com>
2020-12-09 12:43:01 +0100

    landing page

---
+++ script/main.js
@@ -0,0 +1,17 @@
+
+
+//fare un toggle per Burger-menu & navigation-list
+function myFunction(){
+     document.querySelector("#line-1").classList.toggle("change1");
+     document.querySelector("#line-2").classList.toggle("change2");
+     document.querySelector("#line-3").classList.toggle("change3");
+
+     //navigation-list
+     document.querySelector(".navvv") .classList.toggle("change");
+     //paragrafo display none
+     document.querySelector(".paragrafo p").classList.toggle("changep");
+   
+    
+    
+     
+}
